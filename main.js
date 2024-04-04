7bd47ec0a43d89439722d8284a27689eec9fd80b
hassankhan136373 <164183678+hassankhan136373@users.noreply.github.com>
2024-04-04 16:04:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var Name = "hassan";
+//this save name 
+console.log("hello ".concat(Name, ", would do you like to learn some Typescripct today?"));
+//this show the message 
---
+++ main.ts
@@ -0,0 +1,7 @@
+let Name: string = "hassan"; 
+
+//this save name 
+
+
+console.log(`hello ${Name}, would do you like to learn some Typescripct today?`);
+//this show the message 
